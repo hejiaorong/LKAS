bd58f6e9e5b1818d54a564db9dc749b822947c54
于晓阳 <yxy912044255@qq.com>
2022-01-20 17:48:05 +0800

    输出SW6MIL测试模型-2022年1月20日
    1、输出SW6 MIL测试模型-2022年1月20日
    2、修改MIL保护模型测试文件
    3、修改MIL非保护模型测试文件
    主要为：1）自动设置sample time = 0.001，自动设置hd偏差0
    2）自动删除观测CAN，
    3）自动添加到project

--- Script/Model_NoProtect_Script.m
+++ Script/Model_NoProtect_Script.m
@@ -47,12 +47,109 @@ delete_line(filename,Runnable.Outport,Subsystem.Trigger);  % 删除相应线段
 delete_block(strcat(filename,'/','Runnable_',module_name,'_Step')); % 删除runnable端口
 delete_block(strcat(SubSystName,'/function'));
 fprintf('2.您的模型[%s.slx]文件已经完成Simulink属性配置并删除Runnable模块\n',filename);
+
+delete_block(strcat(SubSystName,'/LKASve_g_ob5H_10'));
+delete_block(strcat(SubSystName,'/LKASve_g_ob5L_10'));
+delete_block(strcat(SubSystName,'/LKASve_g_ob6H_10'));
+delete_block(strcat(SubSystName,'/LKASve_g_ob6L_10'));
+delete_block(strcat(SubSystName,'/LKASve_g_ob07H_100'));
+delete_block(strcat(SubSystName,'/LKASve_g_ob07L_100'));
+delete_block(strcat(SubSystName,'/LKASve_g_ob08H_100'));
+delete_block(strcat(SubSystName,'/LKASve_g_ob08L_100'));
+delete_block(strcat(SubSystName,'/Monitor'));
+delete_block(strcat(filename,'/LKASve_g_ob5H_10'));
+delete_block(strcat(filename,'/LKASve_g_ob5L_10'));
+delete_block(strcat(filename,'/LKASve_g_ob6H_10'));
+delete_block(strcat(filename,'/LKASve_g_ob6L_10'));
+delete_block(strcat(filename,'/LKASve_g_ob07H_100'));
+delete_block(strcat(filename,'/LKASve_g_ob07L_100'));
+delete_block(strcat(filename,'/LKASve_g_ob08H_100'));
+delete_block(strcat(filename,'/LKASve_g_ob08L_100'));
+
 %% 3.输出slxp模型，输出mat文件
 getProtectFileName = strcat(protectfile,'.slx');
 getProtectFileName1 = strcat(protectfile,'.slxc');
 getProtectFileName2 = strcat(protectfile,'.slxp');
 getProtectFileName3 = strcat(protectfile,'.mat');
 mdlWks = get_param(filename,'ModelWorkspace');
+
+ob_LKA_Disable_Reason = Simulink.Signal;
+ob_LKA_Disable_Reason.CoderInfo.StorageClass = 'Auto';
+ob_LKA_Disable_Reason.Description = '';
+ob_LKA_Disable_Reason.DataType = 'single';
+ob_LKA_Disable_Reason.Min = [];
+ob_LKA_Disable_Reason.Max = [];
+ob_LKA_Disable_Reason.DocUnits = '';
+ob_LKA_Disable_Reason.Dimensions = -1;
+ob_LKA_Disable_Reason.DimensionsMode = 'auto';
+ob_LKA_Disable_Reason.Complexity = 'auto';
+ob_LKA_Disable_Reason.SampleTime = -1;
+ob_LKA_Disable_Reason.InitialValue = '';
+
+ob_LKA_LKADeactvCSyn = Simulink.Signal;
+ob_LKA_LKADeactvCSyn.CoderInfo.StorageClass = 'Auto';
+ob_LKA_LKADeactvCSyn.Description = '';
+ob_LKA_LKADeactvCSyn.DataType = 'single';
+ob_LKA_LKADeactvCSyn.Min = [];
+ob_LKA_LKADeactvCSyn.Max = [];
+ob_LKA_LKADeactvCSyn.DocUnits = '';
+ob_LKA_LKADeactvCSyn.Dimensions = -1;
+ob_LKA_LKADeactvCSyn.DimensionsMode = 'auto';
+ob_LKA_LKADeactvCSyn.Complexity = 'auto';
+ob_LKA_LKADeactvCSyn.SampleTime = -1;
+ob_LKA_LKADeactvCSyn.InitialValue = '';
+
+ob_LKA_Version = Simulink.Signal;
+ob_LKA_Version.CoderInfo.StorageClass = 'Auto';
+ob_LKA_Version.Description = '';
+ob_LKA_Version.DataType = 'single';
+ob_LKA_Version.Min = [];
+ob_LKA_Version.Max = [];
+ob_LKA_Version.DocUnits = '';
+ob_LKA_Version.Dimensions = -1;
+ob_LKA_Version.DimensionsMode = 'auto';
+ob_LKA_Version.Complexity = 'auto';
+ob_LKA_Version.SampleTime = -1;
+ob_LKA_Version.InitialValue = '';
+
+LKA_SampleTime = Simulink.Parameter;
+LKA_SampleTime.Value = 0.001;
+LKA_SampleTime.CoderInfo.StorageClass = 'Custom';
+LKA_SampleTime.CoderInfo.Alias = '';
+LKA_SampleTime.CoderInfo.Alignment = -1;
+LKA_SampleTime.CoderInfo.CustomStorageClass = 'Const';
+LKA_SampleTime.CoderInfo.CustomAttributes.HeaderFile = '';
+LKA_SampleTime.CoderInfo.CustomAttributes.DefinitionFile = ...
+  'LKAS_calib.c';
+LKA_SampleTime.CoderInfo.CustomAttributes.Owner = '';
+LKA_SampleTime.Description = '';
+LKA_SampleTime.DataType = 'single';
+LKA_SampleTime.Min = [];
+LKA_SampleTime.Max = [];
+LKA_SampleTime.DocUnits = '';
+
+LL_CompHdAg_C = Simulink.Parameter;
+LL_CompHdAg_C.Value = 0;
+LL_CompHdAg_C.CoderInfo.StorageClass = 'Custom';
+LL_CompHdAg_C.CoderInfo.Alias = '';
+LL_CompHdAg_C.CoderInfo.Alignment = -1;
+LL_CompHdAg_C.CoderInfo.CustomStorageClass = 'Const';
+LL_CompHdAg_C.CoderInfo.CustomAttributes.HeaderFile = '';
+LL_CompHdAg_C.CoderInfo.CustomAttributes.DefinitionFile = ...
+  'LKAS_calib.c';
+LL_CompHdAg_C.CoderInfo.CustomAttributes.Owner = '';
+LL_CompHdAg_C.Description = '';
+LL_CompHdAg_C.DataType = 'single';
+LL_CompHdAg_C.Min = [];
+LL_CompHdAg_C.Max = [];
+LL_CompHdAg_C.DocUnits = '';
+
+assignin(mdlWks, 'ob_LKA_Disable_Reason', ob_LKA_Disable_Reason);
+assignin(mdlWks, 'ob_LKA_LKADeactvCSyn', ob_LKA_LKADeactvCSyn);
+assignin(mdlWks, 'ob_LKA_Version', ob_LKA_Version);
+assignin(mdlWks, 'LKA_SampleTime', LKA_SampleTime);
+assignin(mdlWks, 'LL_CompHdAg_C', LL_CompHdAg_C);
+
 save(mdlWks,getProtectFileName3);
 save_system(filename,protectfile);
 try
@@ -70,10 +167,11 @@ zipfilename = protectfile;
 zipfilenames = {getProtectFileName,getProtectFileName3,'Rte_Type.m'};
 zip(zipfilename,zipfilenames);
 movefile(strcat(zipfilename,'.zip'),ModelSavePath); 
-delete(getProtectFileName);
 delete(getProtectFileName2);
 delete(getProtectFileName3);
 proj = simulinkproject;
 addFile(proj,strcat(ModelSavePath,'\',zipfilename,'.zip'));
+addFile(proj,strcat(ModelSavePath,'\',newfile,'.slx'));
+delete(getProtectFileName);
 fprintf('您的NoProtectMIL模型已经输出至[%s%s]\n',ModelSavePath,strcat('\',zipfilename,'.zip'));
 eval('clear all');
